8169a5711414f0c14ab0ee320ef4bf857515583b
miillaadddddd <milad7212@gmail.com>
2022-05-17 23:30:11 +0430

    Add loginPage

---
+++ pages/login/index.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="page-content mt-6 pb-2 mb-10">
+    <div className="container">
+      <div className="login-popup">
+        <div className="form-box">
+          <div className="tab tab-nav-simple tab-nav-boxed form-tab">
+            <ul className="nav nav-tabs nav-fill align-items-center border-no justify-content-center mb-5" role="tablist">
+              <li className="nav-item">
+                <a className="nav-link active border-no lh-1 ls-normal" href="#signin">Login</a>
+              </li>
+              <li className="delimiter">or</li>
+              <li className="nav-item">
+                <a className="nav-link border-no lh-1 ls-normal" href="#register">Register</a>
+              </li>
+            </ul>
+            <div className="tab-content">
+              <div className="tab-pane active" id="signin">
+                <form action="#">
+                  <div className="form-group mb-3">
+                    <input type="text" className="form-control" id="singin-email" name="singin-email" placeholder="Username or Email Address *" required />
+                  </div>
+                  <div className="form-group">
+                    <input type="password" className="form-control" id="singin-password" name="singin-password" placeholder="Password *" required />
+                  </div>
+                  <div className="form-footer">
+                    <div className="form-checkbox">
+                      <input type="checkbox" className="custom-checkbox" id="signin-remember" name="signin-remember" />
+                      <label className="form-control-label" htmlFor="signin-remember">Remember
+                        me</label>
+                    </div>
+                    <a href="#" className="lost-link">Lost your password?</a>
+                  </div>
+                  <button className="btn btn-dark btn-block btn-rounded" type="submit">Login</button>
+                </form>
+                <div className="form-choice text-center">
+                  <label className="ls-m">or Login With</label>
+                  <div className="social-links">
+                    <a href="#" className="social-link social-google fab fa-google border-no" />
+                    <a href="#" className="social-link social-facebook fab fa-facebook-f border-no" />
+                    <a href="#" className="social-link social-twitter fab fa-twitter border-no" />
+                  </div>
+                </div>
+              </div>
+              <div className="tab-pane" id="register">
+                <form action="#">
+                  <div className="form-group">
+                    <input type="email" className="form-control" id="register-email" name="register-email" placeholder="Your Email address *" required />
+                  </div>
+                  <div className="form-group">
+                    <input type="password" className="form-control" id="register-password" name="register-password" placeholder="Password *" required />
+                  </div>
+                  <div className="form-footer">
+                    <div className="form-checkbox">
+                      <input type="checkbox" className="custom-checkbox" id="register-agree" name="register-agree" required />
+                      <label className="form-control-label" htmlFor="register-agree">I agree to
+                        the
+                        privacy policy</label>
+                    </div>
+                  </div>
+                  <button className="btn btn-dark btn-block btn-rounded" type="submit">Register</button>
+                </form>
+                <div className="form-choice text-center">
+                  <label className="ls-m">or Register With</label>
+                  <div className="social-links">
+                    <a href="#" className="social-link social-google fab fa-google border-no" />
+                    <a href="#" className="social-link social-facebook fab fa-facebook-f border-no" />
+                    <a href="#" className="social-link social-twitter fab fa-twitter border-no" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Login
