695fd10a84c4636dcb3273031dd0f5cd690cf305
miillaadddddd <milad7212@gmail.com>
2022-05-18 10:39:41 +0430

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f45231e52de32ab8"; }
+/******/ 		__webpack_require__.h = function() { return "756628c3916b2d4b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
